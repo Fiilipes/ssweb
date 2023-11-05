045aa7fb1adab6ebfa93027d1fd0a84e4bd9a674
your name goes here <jarolimfilip07@gmail.com>
2023-11-05 22:06:43 +0100

    piškvorky základ, nextauth update, header update

--- app/(auth)/api/auth/[...nextauth]/route.jsx
+++ app/(auth)/api/auth/[...nextauth]/route.jsx
@@ -9,7 +9,6 @@ import { authOptions } from "@/utils/authOptions";
 const handler = NextAuth(authOptions);
 export {
     handler as GET,
-    handler as POST,
-    authOptions
+    handler as POST
 }
 
